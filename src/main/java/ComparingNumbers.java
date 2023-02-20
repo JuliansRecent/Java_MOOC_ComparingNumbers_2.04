62fcfa224be9205bc8b1b00413a6903dd2d26440
JuliansRecent <juliansrecent@gmail.com>
2023-02-19 16:04:06 -0800

    Module complete. Created a terminal based application that takes two integers as input, then compares the relation of the values given.

--- src/main/java/ComparingNumbers.java
+++ src/main/java/ComparingNumbers.java
@@ -6,5 +6,17 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Prompt the user for the first number
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        // Prompt the user for the second number
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        // Conditionally print a sentence stating the relation of the first number to the second
+        if (firstNum > secondNum) {
+            System.out.println(firstNum + " is greater than " + secondNum + ".");
+        } else if (firstNum < secondNum) {
+            System.out.println(firstNum + " is smaller than " + secondNum + ".");
+        } else {
+            System.out.println(firstNum + " is equal to " + secondNum + ".");
+        }
     }
 }
